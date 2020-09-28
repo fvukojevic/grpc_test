eef38927ff0a5678f8dc5d46be82f05cb7a17275
Ferdo Vukojevic <f.vukojevic@globalsoft.ba>
2020-09-28 11:07:05 +0200

    grpc highscore logic

--- m-apis/m-highscore/internal/server/grpc/grpc.go
+++ m-apis/m-highscore/internal/server/grpc/grpc.go
@@ -1,16 +1,32 @@
 package grpc
 
 import (
-	pbhighscore "github.com/fvukojevic/grpc_test/"
 	"context"
+	v1highscore "github.com/fvukojevic/grpc_test/m-apis/m-highscore/v1"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
 
 type Grpc struct {
-	address string //where the grpc will listen at
-	srv *grpc.Server
+	address string //address where the gRPC will listen at
+	srv     *grpc.Server
 }
 
-func (g *Grpc) SetHighScore(ctx context.Context, input *SetHighScoreRequest) (*SetHighScoreResponse, error) {
+var HighScore = 999999.0
 
+func (g *Grpc) SetHighScore(ctx context.Context, input *v1highscore.SetHighScoreRequest) (*v1highscore.SetHighScoreResponse, error) {
+	log.Info().Msg("SetHighscore in m-highscore is called")
+
+	HighScore = input.HighScore
+	return &v1highscore.SetHighScoreResponse{
+		Set: true,
+	}, nil
+}
+
+func (g *Grpc) GetHighScore(ctx context.Context, input *v1highscore.GetHighScoreRequest) (*v1highscore.GetHighScoreResponse, error) {
+	log.Info().Msg("GetHighscore in m-highscore is called")
+
+	return &v1highscore.GetHighScoreResponse{
+		HighScore: HighScore,
+	}, nil
 }
